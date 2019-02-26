7d85ee3c8afe24a2bdff67235e253a430faa4615
HurlyDesousa <34354667+HurlyDesousa@users.noreply.github.com>
2019-02-26 21:36:58 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 //Prologue of footer.php:
 //This page creates the footer that is present at the top of each page.
 
-echo '<div class="footer-container"><p class="footer">&copy; Copyright 2018</p></div>';
+echo '<div class="footer-container"><p class="footer">&copy; Copyright <script>document.write(new Date().getFullYear())</script></p></div>';
 // The footer will display at the bottom of each page and provide copyright information.
 
 ?>
